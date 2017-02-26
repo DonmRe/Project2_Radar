82b0de53cb4ed83544a77a3498048417b5a1a17d
alex espinoza <alexjespinozac@gmail.com>
2017-02-25 19:39:59 -0800

    trying to set up new post form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,40 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  def index
+    @posts = Post.all
+  end
+  def show
+  end
+  def new
+  end
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+  def edit
+  end
+  def update
+    if @post.update_attributes(post_params)
+      redirect_to :posts
+    else
+      render :edit
+    end
+  end
+  def destroy
+    @post.destroy
+    redirect_to posts_path
+  end
+  
+private
+  def post_params
+    params.require(:post).permit(:date, :subject, :data).merge(:location_id => params[:id], :topic_id => params[:id])
+  end
+  def set_post
+    @post = Post.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #posts
+  root "posts#index"
+  get "posts/" => "posts#index"
+  get "posts/new" => "posts/new", as: :new_post
+  get "posts/:id" => "posts#show", as: :post
+
+  #sessions
+  get "/login" => "sessions#new"
+  post "/login" => "sessions#create"
+  delete "/logout" => "sessions#destroy"
 end
